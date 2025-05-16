2c7784f718bcdaadf833b379a46ba044a1e945a3
Batysab1 <elbatysa@gmail.com>
2025-05-16 08:21:26 -0300

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -279,3 +279,11 @@ export default function Home() {
     </div>
   )
 }
+import { createClient } from '@/utils/supabase/server';
+
+export default async function Notes() {
+  const supabase = await createClient();
+  const { data: notes } = await supabase.from("notes").select();
+
+  return <pre>{JSON.stringify(notes, null, 2)}</pre>
+}
